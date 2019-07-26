93ce4440f7a4a8a7b07d0ac0bae05ef7e2570eab
Maximilian Kempa <maximilian.kempa@gmx.de>
2019-07-26 17:51:08 +0200

    update package requirements

--- package_requirements.sh
+++ package_requirements.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 apt-get update  # To get the latest package lists
-apt-get install ros-kinetic-map-server python-wstool python-rosdep ninja-build ros-kinetic-abseil-cpp ros-kinetic-geometry2 libarmadillo-dev ros-kinetic-moveit-msgs ros-kinetic-rviz ros-kinetic-cv-bridge ros-kinetic-rgbd-launch libgazebo7-dev ros-kinetic-gazebo-ros-pkgs ros-kinetic-gazebo-ros-control ros-kinetic-ros-controllers ros-kinetic-navigation ros-kinetic-sbg-driver pip -y
+apt-get install ros-kinetic-map-server python-wstool python-rosdep ninja-build ros-kinetic-abseil-cpp ros-kinetic-geometry2 libarmadillo-dev ros-kinetic-moveit-msgs ros-kinetic-rviz ros-kinetic-cv-bridge ros-kinetic-rgbd-launch libgazebo7-dev ros-kinetic-gazebo-ros-pkgs ros-kinetic-gazebo-ros-control ros-kinetic-ros-controllers ros-kinetic-navigation ros-kinetic-sbg-driver -y
 #etc.
 
