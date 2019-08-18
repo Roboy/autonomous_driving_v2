6933bfd5a64dddccdfa0cdcb330c197ecb01b06b
DominikMuhle <dominikmuhle@icloud.com>
2019-08-18 10:26:14 +0200

    fixed calibration script

--- roboy_navigation/angle_sensor_calibration.py
+++ roboy_navigation/angle_sensor_calibration.py
@@ -4,6 +4,8 @@ import rospy
 import rospkg
 import numpy as np
 import os
+import yaml
+from roboy_middleware_msgs.msg import MotorAngle
 
 class AngleSensorCalibration:
 
@@ -19,14 +21,17 @@ class AngleSensorCalibration:
         self.raw = {}
 
     def start(self):
+        def callback(msg):
+            self.data.append(msg.raw_angles[0])
+            self.counter += 1
+
         rospy.init_node('angle_calibration', anonymous=True)
         for angle in self.target_angles:
             print('Move the rickshaw to the {}'.format(angle))
             try:
-                input_angle = input('Which angle is this? \n Please specify the angle. If you want to take the maximum angle of {} press enter'.format(self.default_angles[angle]))
-            except SyntaxError:
-                input_angle = self.default_angles[angle]
-            if type(angle) != int:
+                input_angle = int(input('Which angle is this? \n Please specify the angle. If you want to take the maximum angle of {} press enter '.format(self.default_angles[angle])))
+            except SyntaxError or ValueError:
+                print('Going on with default value.')
                 input_angle = self.default_angles[angle]
             try:
                 input('If rickshaw is in position press Enter...')
@@ -35,21 +40,21 @@ class AngleSensorCalibration:
 
             self.angles[angle] = input_angle
 
-            data = ()
-            for i in range(num_msgs):
-                data = rospy.wait_for_message('/roboy/middleware/StearingAngle', MotorAngle)
-                data.append(data.raw_angles[0])
-            collected_data = np.asarray(data)
-            raw_angle = 'raw' + angle
+            self.data = []
+            self.counter = 0
+            while self.counter < self.num_msgs:
+                message = rospy.Subscriber('/roboy/middleware/StearingAngle', MotorAngle, callback)
+                #data.append(message.raw_angles[0])
+            collected_data = np.asarray(self.data)
+            raw_angle = 'raw_' + angle
             self.raw[raw_angle] = int(collected_data.mean())
         calibration = dict(raw = self.raw,
                            angles = self.angles)
 
         with open(os.path.join(self.pkg_path, 'config/calibration.yaml'), 'w') as ymlfile:
-        yaml.dump(calibration, ymlfile, default_flow_style=False)
+            yaml.dump(calibration, ymlfile, default_flow_style=False)
 
-        output = dict()
-        f.open("raw_values", "w")
-        f.write(str(self.raw))
-        f.close()
+
+if __name__=='__main__':
+    AngleSensorCalibration(num_msgs=10).start()
   
