334429576f6068413a859f0184d189481723e7c3
DominikMuhle <50248836+DominikMuhle@users.noreply.github.com>
2019-09-03 14:46:46 +0200

    Angle data from adafruit feather. New topic name

--- roboy_navigation/true_angle.py
+++ roboy_navigation/true_angle.py
@@ -41,7 +41,7 @@ class AngleSensorListener:
                     * (self.right_angle - self.left_angle) * pi / 180
             self.pub.publish(angle)
 
-        rospy.Subscriber('/roboy/middleware/StearingAngle', MotorAngle,
+        rospy.Subscriber('/roboy/middleware/SteeringAngle', MotorAngle,
                          angle_receiver)
         rospy.spin()
 
