ab62b2f82dff9549a37ce5eb14a02aa9fc4da1ed
Maximilian Kempa <maximilian.kempa@gmx.de>
2019-09-11 12:31:55 +0200

    add source code comments for Python scripts in order to create Sphinx documentation)

--- quadtree_planner/scripts/calculateHasArrivedFlag.py
+++ quadtree_planner/scripts/calculateHasArrivedFlag.py
@@ -9,9 +9,27 @@ from move_base_msgs.msg import MoveBaseActionGoal
 from tf import TransformListener
 from std_msgs.msg import Bool
 
-# This script calculates and publishes the hasArrivedFlag
+"""
+.. module:: calculateHasArrivedFlag
+    :platform: ROS
+    :synopsis: module calculates and publishes the hasArrivedFlag
+
+.. moduleauthor:: Maximilian Kempa <maximilian.kempa@tum.de>
+"""
+
 class CalculateHasArrivedFlag:
+    """Class for calculating and publishing hasArrivedFlag
+
+    This class provides some basic methods for calculating and publishing the hasArrivedFlag.
+
+    """
     def __init__(self, maximum_distance_):
+        """A simple initialization method.
+
+        Args:
+            maximum_distance_ (float): maximum distance from goal position in meters that is still
+            acceptable for publishing the hasArrived flag
+        """
         self.goal_x = 0
         self.goal_y = 0
         self.OnlyOneOutputPerGoal = True
@@ -19,6 +37,11 @@ class CalculateHasArrivedFlag:
         self.maximum_velocity = 0.0
         
     def start(self):
+        """Method to create the subscribers to the ego velocity and to the goal position.
+
+        Args:
+            None
+        """
         rospy.init_node('calculateHasArrivedFlag')
         self.tf = TransformListener()
         rospy.loginfo('Node calculateHasArrived is started!')
@@ -30,24 +53,42 @@ class CalculateHasArrivedFlag:
         rospy.spin()
 
     def handle_goal_position(self, goal_pos):
+        """Callback for receiving the goal position
+
+        Args:
+            goal_pos (MoveBaseActionGoal): contains the translation and orientation of the goal position
+        """
         self.OnlyOneOutputPerGoal = True
         self.goal_x, self.goal_y = goal_pos.goal.target_pose.pose.position.x, goal_pos.goal.target_pose.pose.position.y
         rospy.loginfo('Goal Position(x=%.2f, y=%.2f) received', self.goal_x, self.goal_y)
 
     def euclidDistance(self, goal_pos_x, goal_pos_y, current_pos_x, current_pos_y):
-	    return np.sqrt((goal_pos_x - current_pos_x)**2 + (goal_pos_y - current_pos_y)**2)
+        """Method to calculate the euclidean distance between the current position and the goal position
+
+        Args:
+            goal_pos_x (float): x coordinate of goal position in meters
+            goal_pos_y (float): y coordinate of goal position in meters
+            current_pos_x(float): x coordinate of current position in meters
+            current_pos_y(float): y coordinate of current position in meters
+        """
+        return np.sqrt((goal_pos_x - current_pos_x)**2 + (goal_pos_y - current_pos_y)**2)
 
     def handle_ego_velocity(self, cmd_vel):
+        """Callback for receiving the ego velocity
+
+        Args:
+            cmd_vel (float): ego velocity
+        """
         self.ego_velocity = np.sqrt(cmd_vel.linear.x**2 + cmd_vel.linear.y**2)
         t = self.tf.getLatestCommonTime("/map", "/base_link")
-        position, quaternion = self.tf.lookupTransform("/map", "/base_link", t)         
+        position, quaternion = self.tf.lookupTransform("/map", "/base_link", t)
         current_pos_x, current_pos_y, = position[0], position[1]
         euclid_dist = self.euclidDistance(self.goal_x, self.goal_y, current_pos_x, current_pos_y)
-        if( (self.ego_velocity <= self.maximum_velocity) and (euclid_dist <= self.maximum_distance) and (self.OnlyOneOutputPerGoal == True) ):
-			self.OnlyOneOutputPerGoal = False
-			rospy.sleep(3)
-			self.flag_publisher.publish(True)
-			rospy.loginfo('Publishing hasArrived = True')
+        if self.ego_velocity <= self.maximum_velocity and euclid_dist <= self.maximum_distance and self.OnlyOneOutputPerGoal:
+            self.OnlyOneOutputPerGoal = False
+            rospy.sleep(3)
+            self.flag_publisher.publish(True)
+            rospy.loginfo('Publishing hasArrived = True')
         else:
             pass  
 
--- quadtree_planner/scripts/luigi_test_client.py
+++ quadtree_planner/scripts/luigi_test_client.py
@@ -5,8 +5,22 @@ import argparse
 import time
 from roboy_cognition_msgs.srv import DriveToLocation
 
-# This script is supposed to be used only as test script for the communication between Luigi and autononomous driving via the service DriveToLocation (located in roboy_cognition_msgs)
+"""
+.. module:: luigi_test_client
+    :platform: ROS
+    :synopsis:  script is supposed to be used only as test script for the communication between 
+    Luigi and autononomous driving via the service DriveToLocation (located in roboy_cognition_msgs)
+
+.. moduleauthor:: Maximilian Kempa <maximilian.kempa@tum.de>
+"""
+
+
 def ad_communication(location):
+    """Method to simulate a location request from luigi
+
+    Args:
+        location (string): string that describes the location where Luigi wants the rickshaw drive to
+    """
     rospy.wait_for_service('autonomous_driving')
     try:
         drive_to_location = rospy.ServiceProxy('autonomous_driving', DriveToLocation)
@@ -14,8 +28,7 @@ def ad_communication(location):
         return response.eta, response.error_message, response.path_found
     except rospy.ROSInterruptException as e:
         print('Service call failed:', e)
-# If driving module is run without ROS, comment everything from above (including imports) and uncomment this:
-    # return 42, ""
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Define goal location from datatype string")
